bc5be7bb133e51727bc0869dca0e399ba85f7356
cphung1 <cphung97@gmail.com>
2020-01-10 09:22:37 +0000

    determines how many 100's are within a given number

---
+++ how-many-hundreds.js
@@ -0,0 +1,12 @@
+function howManyHundreds (number) {
+  let remainder = number % 100;
+  number -= remainder;
+  let numOfHundreds = number / 100
+  return numOfHundreds
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
